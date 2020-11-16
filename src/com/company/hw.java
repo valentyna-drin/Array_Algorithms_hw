38b08ed6b30836cb61e700af57d1d878144fbd63
valya <valentyna.drin@gmail.com>
2020-11-16 14:31:21 +0200

    commit 4

--- src/com/company/hw.java
+++ src/com/company/hw.java
@@ -27,7 +27,7 @@ public class hw {
         System.out.println(found);
     }
 
-    public static void hw2(){
+    public static void hw2() {
         String[] students = {"Alissa", "Ben", "Charlie", "Dianna"};
         String[] grades = {"B", "D", "B", "A"};
         final String key = "Charlie";
@@ -51,14 +51,14 @@ public class hw {
             for (int c = s + 1; c < data.length; c++)
                 if (data[c] < data[smallestInd])
                     smallestInd = c;
-            swap (data, s, smallestInd);
+            swap(data, s, smallestInd);
         }
         for (int e : data)
             System.out.print(e + " ");
         System.out.println("");
     }
 
-    static void swap (int[] array, int ind1, int ind2){
+    static void swap(int[] array, int ind1, int ind2) {
         int tmp = array[ind1];
         array[ind1] = array[ind2];
         array[ind2] = tmp;
@@ -83,10 +83,88 @@ public class hw {
         System.out.println("");
     }
 
-    public static void hw4() {
+    public static void hw4_1() { // sequential search for small array
         String link = "http://www-personal.umich.edu/~jlawler/wordlist";
         String[] words = ReadViaURL.readWords(link);
-        String[] words = new String[];
-
+        String[] words_local = new String[20];
+        int b = 0;
+        for (int i = 0; i < words_local.length; i++) {
+            words_local[i] = words[b];
+            b++;
+        }
+        String key = "abacus";
+        boolean found = false;
+        for (int i = 0; !found && i < words_local.length; i++)
+            if (words_local[i].equals(key))
+                found = true;
+        System.out.println(found);
     }
+
+    public static void hw4_2() { // sequential search for large array
+        String link = "http://www-personal.umich.edu/~jlawler/wordlist";
+        String[] words = ReadViaURL.readWords(link);
+        String[] words_local = new String[6000];
+        int b = 0;
+        for (int i = 0; i < words_local.length; i++) {
+            words_local[i] = words[b];
+            b++;
+        }
+        String key = "abacus";
+        boolean found = false;
+        for (int i = 0; !found && i < words_local.length; i++)
+            if (words_local[i].equals(key))
+                found = true;
+        System.out.println(found);
+    }
+
+    public static void hw4_3() { //binary search for small array
+        String link = "http://www-personal.umich.edu/~jlawler/wordlist";
+        String[] words = ReadViaURL.readWords(link);
+        String[] words_local = new String[20];
+        int b = 0;
+        for (int i = 0; i < words_local.length; i++) {
+            words_local[i] = words[b];
+            b++;
+        }
+        String key = "abacus";
+        int lower_index = 0, higher_index = words_local.length - 1;
+        boolean found = false;
+        while (!found && (lower_index <= higher_index)) {
+            int middle_index = (lower_index + higher_index) / 2;
+            String middle_value = words_local[middle_index];
+            if (middle_value.compareTo(key) == 0)
+                found = true;
+            else if (middle_value.compareTo(key) > 0)
+                higher_index = middle_index - 1;
+            else
+                lower_index = middle_index + 1;
+        }
+        System.out.println(found);
+    }
+
+    public static void hw4_4() { //binary search for large array
+        String link = "http://www-personal.umich.edu/~jlawler/wordlist";
+        String[] words = ReadViaURL.readWords(link);
+        String[] words_local = new String[6000];
+        int b = 0;
+        for (int i = 0; i < words_local.length; i++) {
+            words_local[i] = words[b];
+            b++;
+        }
+        String key = "abacus";
+        int lower_index = 0, higher_index = words_local.length - 1;
+        boolean found = false;
+        while (!found && (lower_index <= higher_index)) {
+            int middle_index = (lower_index + higher_index) / 2;
+            String middle_value = words_local[middle_index];
+            if (middle_value.compareTo(key) == 0)
+                found = true;
+            else if (middle_value.compareTo(key) > 0)
+                higher_index = middle_index - 1;
+            else
+                lower_index = middle_index + 1;
+        }
+        System.out.println(found);
+    }
+
 }
